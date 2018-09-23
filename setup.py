3a8a60ee73e1cec98c59a0071686ac6368803b9a
Nir <nir@nirharel.space>
2018-09-23 09:54:36 +0300

    updated version (minor)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-launch-library",
-    version="0.1.4",
+    version="0.2",
     author="Nir Harel",
     author_email="nir@nirharel.space",
     description="A wrapper for the launchlibrary.net API",
